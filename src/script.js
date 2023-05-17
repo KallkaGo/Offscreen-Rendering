501bfd6e87bd0b028685daa865f147338504faed
KallkaGo <765830637@qq.com>
2023-05-18 00:47:14 +0800

    add:增加相框以及灯光

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as CameraUtils from 'three/examples/jsm/utils/CameraUtils'
 import * as dat from 'lil-gui'
 
 
@@ -20,6 +21,12 @@ scene.background = new THREE.Color('ivory')
 
 const buffer = new THREE.WebGLRenderTarget(1000, 1000)
 
+/* 
+bufferCamera
+*/
+const bufferCamera = new THREE.PerspectiveCamera(75, 1.0, 0.01, 100)
+bufferCamera.position.set(0.25, - 0.25, 2)
+
 
 /**
  * Test mesh
@@ -33,12 +40,42 @@ const material = new THREE.MeshToonMaterial({
 })
 
 
+/* 
+border
+*/
+
+const TBGeo = new THREE.BoxGeometry(2.2, 0.1, 0.1)
+const LRGeo = new THREE.BoxGeometry(0.1, 2, 0.1)
+
+const borderMaterial = new THREE.MeshToonMaterial({ color: 'gray' })
+
+const group = new THREE.Group()
+
+const leftBorder = new THREE.Mesh(LRGeo, borderMaterial)
+leftBorder.position.set(-1.05, 0, 0.05)
+
+const rightBorder = new THREE.Mesh(LRGeo, borderMaterial)
+rightBorder.position.set(1.05, 0, 0.05)
+
+const topBorder = new THREE.Mesh(TBGeo, borderMaterial)
+topBorder.position.set(0, 1.05, 0.05)
+
+const bottomBorder = new THREE.Mesh(TBGeo, borderMaterial)
+bottomBorder.position.set(0, -1.05, 0.05)
+
+group.add(leftBorder, rightBorder, topBorder, bottomBorder)
+
+scene.add(group)
+
 
 const bufferScene = new THREE.Scene()
 
-const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshStandardMaterial(
+bufferScene.add(bufferCamera)
+
+const cube = new THREE.Mesh(new THREE.BoxGeometry(0.3, 0.3, 0.3), new THREE.MeshPhongMaterial(
     { color: 'red' })
 )
+cube.rotation.y += Math.PI / 6
 
 bufferScene.add(cube)
 
@@ -49,7 +86,7 @@ bufferSceneLight
 const bufferambientLight = new THREE.AmbientLight('black')
 bufferScene.add(bufferambientLight)
 
-const bufferDirlight = new THREE.DirectionalLight()
+const bufferDirlight = new THREE.DirectionalLight('white', 0.6)
 bufferDirlight.position.set(0, 5, 5)
 bufferScene.add(bufferDirlight)
 
@@ -65,7 +102,7 @@ Light
 const ambientLight = new THREE.AmbientLight()
 scene.add(ambientLight)
 
-const directionalLgiht = new THREE.DirectionalLight()
+const directionalLgiht = new THREE.DirectionalLight('white', 0.7)
 directionalLgiht.position.set(0, 5, 5)
 scene.add(directionalLgiht)
 
@@ -95,8 +132,8 @@ window.addEventListener('resize', () => {
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0.25, - 0.25, 1)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
+camera.position.set(0.25, - 0.25, 2)
 scene.add(camera)
 
 // Controls
@@ -107,7 +144,8 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -115,15 +153,28 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
+let bottomLeftCorner = new THREE.Vector3()
+    , bottomRightCorner = new THREE.Vector3()
+    , topLeftCorner = new THREE.Vector3()
+
 const tick = () => {
     // Update controls
     controls.update()
 
-    cube.rotation.y += 0.001
+    // cube.rotation.y += 0.01
+
+    bufferCamera.position.copy(camera.position)
+    bufferCamera.lookAt(controls.target)
+    bufferScene.localToWorld(bottomLeftCorner.set(-1, -1, 0))
+    bufferScene.localToWorld(bottomRightCorner.set(1, - 1, 0))
+    bufferScene.localToWorld(topLeftCorner.set(-1, 1, 0))
+    // set the projection matrix to encompass the portal's frame
+    CameraUtils.frameCorners(bufferCamera, bottomLeftCorner, bottomRightCorner, topLeftCorner, false)
+
 
     // Render
     renderer.setRenderTarget(buffer)
-    renderer.render(bufferScene, camera)
+    renderer.render(bufferScene, bufferCamera)
     renderer.setRenderTarget(null)
     renderer.render(scene, camera)
 
